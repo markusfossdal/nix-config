0b9ad3dae75d7fb9ea25d3fbfd4c3f874fc547ad
Markus Fossdal <mf@dhcp-10-24-83-217.wlan.ntnu.no>
2025-04-16 10:20:56 +0200

    lazygit test commit

--- configuration/home/sh.nix
+++ configuration/home/sh.nix
@@ -17,9 +17,9 @@
   };
 
   home.shellAliases = {
+    fzfgrep = ''nvim "+$(rg --line-number --no-heading . | fzf --preview "bat --style=numbers --color=always {1} --highlight-line {2}" --delimiter : --nth 3.. | cut -d: -f1,2)"'';
     cat = ''bat'';
     fzfnvim = ''nvim "$(find . -type f | fzf --preview 'bat --style=numbers --color=always {}')" '';
     fzfview = ''find . -type f | fzf --preview 'bat --style=numbers --color=always {}' '';
-    fzfgrepnvim = ''nvim "$(grep -rn . | fzf --preview "bat --style=numbers --color=always {1} --highlight-line {2}" --delimiter : --nth 3.. | cut -d: -f1,2)"'';
   };
 }
