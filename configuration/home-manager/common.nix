53e4d19512d2e4b1f73bd30af88e222475f8cc02
Markus Fossdal <mf@dhcp-10-24-80-180.wlan.ntnu.no>
2025-05-08 13:54:09 +0200

    removed accesibility toggle in darwin/system.nix due to error

--- configuration/home-manager/common.nix
+++ configuration/home-manager/common.nix
@@ -10,8 +10,63 @@
   # programs.rust.enable = true;
   # programs.neovim.enable = true;
 
-  programs.alacritty.enable = true;
-  # programs.yazi.enable = true;
+  # programs.alacritty = {
+  #   enable = true;
+  # };
+
+    programs.alacritty = {
+    enable = true;
+    settings = {
+      window = {
+        opacity = 0.9;
+        padding = { x = 5; y = 5; };
+      };
+
+      font.normal.family = "FiraCode Nerd Font";
+
+      colors = {
+        primary = {
+          background = "0x1e1e2e";
+          foreground = "0xcdd6f4";
+        };
+        normal = {
+          black   = "0x45475a";
+          red     = "0xf38ba8";
+          green   = "0xa6e3a1";
+          yellow  = "0xf9e2af";
+          blue    = "0x89b4fa";
+          magenta = "0xf5c2e7";
+          cyan    = "0x94e2d5";
+          white   = "0xbac2de";
+        };
+        bright = {
+          black   = "0x585b70";
+          red     = "0xf38ba8";
+          green   = "0xa6e3a1";
+          yellow  = "0xf9e2af";
+          blue    = "0x89b4fa";
+          magenta = "0xf5c2e7";
+          cyan    = "0x94e2d5";
+          white   = "0xa6adc8";
+        };
+      };
+
+      key_bindings = [
+        {
+          key = "N";
+          mods = "Control|Shift";
+          action = "CreateNewWindow";
+        }
+        {
+          key = "Q";
+          mods = "Control|Shift";
+          action = "Quit";
+        }
+      ];
+    };
+  };
+
+  # programs.alacritty.enable = true;
 
   home.packages = with pkgs; [
     yazi
